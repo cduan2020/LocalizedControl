0c7c6f4f239a91eb20c22a53406a6d8884fde6c6
cduan2020 <63173470+cduan2020@users.noreply.github.com>
2020-07-13 09:27:31 -0500

    Update pf2control.m

--- Power grid/pf2control.m
+++ Power grid/pf2control.m
@@ -21,13 +21,8 @@ V=ps.bus(:,8);
 theta=ps.bus(:,9)*pi/180;
 Vsol=V.*exp(1j*theta);
 
-%%%%%%%%%%Initialize Machine Variables%%%%%%%%%%%%%%%%%%
-xd=ps.gen_dyn(:,1)*0;
-Ra=ps.gen_dyn(:,1)*0;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 
 %%%%%%%%%%Initialize Network Variables%%%%%%%%%%%%%%%%%%
-Yg=1./(Ra+1j*xd);
 MM=full(sparse(genbus,1:ng,ones(ng,1),nb,ng));
 E = ((MM'*V + Qg .* xd ./ (MM'*V)) + 1j * (Pg .* xd ./ (MM'*V))) .* exp(1j * MM'*theta);
 Vref=abs(E);
@@ -35,11 +30,7 @@ delta = angle(E./Vref);
 delta=delta-delta(refgen);
 Yd=conj((Pd+1j*Qd)./Vsol)./Vsol;
 Ywithload=ps.Y+sparse(1:nb,1:nb,Yd); 
-Yreduce= Ywithload(genbus,genbus)-Ywithload(genbus,loadbus)*(Ywithload(loadbus,loadbus)\Ywithload(loadbus,genbus));
-
-% YredWithGen=Yreduce*inv(Yreduce+diag(Yg))*diag(Yg);
-
-YredWithGen=Yreduce;
+YredWithGen= Ywithload(genbus,genbus)-Ywithload(genbus,loadbus)*(Ywithload(loadbus,loadbus)\Ywithload(loadbus,genbus));
 Pm=real(diag(Vref.*exp(1j*delta))*conj(YredWithGen)*diag(exp(-1j*delta))*Vref);
 norm(Pm-Pg,inf)
 
